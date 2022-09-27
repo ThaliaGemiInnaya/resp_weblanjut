e90bd3c24469fb2bdb23e0da0740d29859ae0922
ThaliaGemiInnaya <83534462+ThaliaGemiInnaya@users.noreply.github.com>
2022-09-27 22:51:31 +0700

    resp_weblanjut

---
+++ 2017051025_prakweblanjut5/app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<?= $this->extend('templates/template'); ?>
+<?= $this->section('content'); ?>
+
+
+<h1>Thalia Gemi Innaya</h1>
+<p>2017051025</p>
+
+<?= $this->endSection(); ?>
---
+++ 2017051025_prakweblanjut5/app/Views/pages/home.php
@@ -0,0 +1,9 @@
+<?= $this->extend('templates/template'); ?>
+<?= $this->section('content'); ?>
+
+
+<h1>HOME</h1>
+<p>Halo aku Thalia</p>
+
+
+<?= $this->endSection(); ?>
